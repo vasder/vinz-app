bd23e87478a9f3daeb903815f943009dca705216
vineeth <vinithktp@gmail.com>
2017-11-28 09:17:38 +0530

    Updated
    Updated

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { EmployeeService } from '../../employee.service';
 
 @Component({
   selector: 'app-menu',
---
+++ src/app/employee.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Subject } from 'rxjs/Subject';
+
+import { Employee } from './models/employee';
+
+@Injectable()
+export class EmployeeService {
+    employeeListEvent: Subject<Employee[]> = new Subject<Employee[]>();
+    constructor(private http: HttpClient) {
+
+    }
+
+
+    getEmployees() {
+        return this.http
+            .get('/assets/employee.json')
+            .map((employees: Array<Employee>) => {
+                employees.push({
+                    name: 'Raja',
+                    age: 22
+                });
+                this.employeeListEvent.next(employees);
+                return employees;
+            });
+    }
+}
